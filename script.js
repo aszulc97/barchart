d7f403c6436b8d8ed865216f354773b66403edda
Agata Szulc <agataszulc97@gmail.com>
2022-02-11 11:53:26 +0100

    smooth removing animation

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ function createBar() {
   list.appendChild(li);
   console.log(allBars);
 
-  if (allBars.length === 40) {
+  if (allBars.length === 10) {
     allBars.shift();
-    list.querySelector("li").remove();
+    list.style.animation = "move 0.5s";
+    list.addEventListener("animationend", () => (list.style.animation = "none"));
+    list.querySelector("li").style.animation = "fadeOut 0.3s";
+    list
+      .querySelector("li")
+      .addEventListener("animationend", () => list.querySelector("li").remove());
+    // list.querySelector("li").remove();
   }
-  setTimeout(() => createBar(), 500);
+  setTimeout(() => createBar(), 1000);
 }
 
 function getNumberOfCustomer() {
